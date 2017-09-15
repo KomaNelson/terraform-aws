ffa37f3e78bb69c7209d6f223e5c58cca9220645
Nelson Koma <koma.nelson@gmail.com>
2017-09-15 09:57:26 +0200

    update bootstrap script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,36 @@
-cd /tmp/ wget https://apt.puppetlabs.com/puppet5-release-jessie.deb
-sudo dpkg -i puppet5-release-jessie.deb
-sudo apt-get update
+#!/usr/bin/env bash
+set -exu
+
+HOSTNAME=%%HOSTNAME%% # terraform replace()
+DOMAIN=ie.p16n.org
+FQDN="$HOSTNAME.$DOMAIN"
+
+# Update the hostname
+sed -ie "s/$(hostname --fqdn)/$FQDN/" /etc/hosts
+sed -ie "s/$(hostname)/$HOSTNAME/" /etc/hosts
+echo "$HOSTNAME" > /etc/hostname
+/etc/init.d/hostname.sh
+
+PUPPET_AGENT_VERSION=5.2.0-1jessie
+
+PUPPET_SERVER=puppet.za.p16n.org
+PUPPET_ENVIRONMENT=ge-aws-dublin
+
+# Add the Puppet repository
+wget -q https://apt.puppetlabs.com/puppet5-release-jessie.deb
+dpkg --install puppet5-release-jessie.deb
+rm puppet5-release-jessie.deb
+apt-get update
+
+# Install puppet-agent and configure
+apt-get install -y puppet-agent=$PUPPET_AGENT_VERSION
+cat <<EOF >> /etc/puppetlabs/puppet/puppet.conf
+[agent]
+server = $PUPPET_SERVER
+environment = $PUPPET_ENVIRONMENT
+EOF
+
+# Run the Puppet agent
+PATH="/opt/puppetlabs/bin:$PATH"
+puppet agent --enable
+puppet agent --test --waitforcert
